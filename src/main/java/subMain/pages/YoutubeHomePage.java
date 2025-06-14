951755f43fab4ee5c257787a9611ce997d8749ed
FlowerWineMonk <hidekitakuya0@gmail.com>
2025-06-14 16:22:44 +0500

    add youtube sign in button for future login use

--- src/main/java/subMain/pages/YoutubeHomePage.java
+++ src/main/java/subMain/pages/YoutubeHomePage.java
@@ -8,12 +8,16 @@ import org.openqa.selenium.support.PageFactory;
 public class YoutubeHomePage extends AbstractPage {
   private static final String YOUTUBE_SEARCH_INPUT_LOCATOR = "//input[@name='search_query']";
   private static final String YOUTUBE_SEARCH_BUTTON_LOCATOR = "//button[@aria-label='Search']";
+  private static final String YOUTUBE_SIGN_IN_BTN_LOCATOR = "//a[@aria-label='Sign in']";
 
   @FindBy(xpath = YOUTUBE_SEARCH_INPUT_LOCATOR)
   private WebElement searchInput;
 
   @FindBy(xpath = YOUTUBE_SEARCH_BUTTON_LOCATOR)
   private WebElement searchButton;
+
+  @FindBy(xpath = YOUTUBE_SIGN_IN_BTN_LOCATOR)
+  private WebElement signInBtn;
 
   public YoutubeHomePage() {
     super();
@@ -23,5 +27,9 @@ public class YoutubeHomePage extends AbstractPage {
   public void searchFor(String query) {
     searchInput.sendKeys(query);
     searchButton.click();
+  }
+
+  public void signInBtnClick() {
+    signInBtn.click();
   }
 }
